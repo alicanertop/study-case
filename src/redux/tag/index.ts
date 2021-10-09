d16449a8ef482cc25c65d2230b776564f979e0dd
Alican ERTOP <alicanertop@gmail.com>
2021-10-09 19:36:05 +0300

    redux tag added

---
+++ src/redux/tag/index.ts
@@ -0,0 +1,46 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+
+import { Status } from '../types/Status'
+import httpService from '../../services/http-service'
+
+export interface TagState {
+  tagList: string[]
+  status: Status
+}
+
+export const initialState: TagState = { tagList: [], status: Status.idle }
+
+// The function below is called a thunk and allows us to perform async logic. It
+// can be dispatched like a regular action: `dispatch(blabal(10))`. This
+// will call the thunk with the `dispatch` function as the first argument. Async
+// code can then be executed and other actions can be dispatched. Thunks are
+// typically used to make async requests.
+export const getAllTags = createAsyncThunk('tag/getAll', async () => {
+  const response = await httpService.get<[]>('/tags')
+  // The value we return becomes the `fulfilled` action payload
+  return response as unknown as string[]
+})
+
+export const tagSlice = createSlice({
+  name: 'tag',
+  initialState,
+  // The `reducers` field lets us define reducers and generate associated actions
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getAllTags.pending, (state) => {
+        state.status = Status.loading
+        state.tagList = []
+      })
+      .addCase(getAllTags.rejected, (state) => {
+        state.status = Status.failed
+        state.tagList = []
+      })
+      .addCase(getAllTags.fulfilled, (state, action) => {
+        state.status = Status.idle
+        state.tagList = action.payload || []
+      })
+  },
+})
+
+export default tagSlice.reducer
