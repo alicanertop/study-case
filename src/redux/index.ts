d6710fa9e0d6dfaafb6fcc0386f833c6f283f52d
Alican ERTOP <alicanertop@gmail.com>
2021-10-09 23:00:43 +0300

    tag, item, itemTypes, manufacturer reducers added redux index

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,6 +1,11 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
 import type { RootState, AppDispatch } from './store'
 
+export * as tagReducer from './tag'
+export * as itemReducer from './item'
+export * as itemTypesReducer from './itemTypes'
+export * as manufacturerReducer from './manufacturer'
+
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch = () => useDispatch<AppDispatch>()
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
