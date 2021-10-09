475eeff7d6e0b816c7c48f928bb56744b45f4b24
Alican ERTOP <alicanertop@gmail.com>
2021-10-09 19:35:45 +0300

    redux manufacturer added

---
+++ src/redux/manufacturer/index.ts
@@ -0,0 +1,47 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+
+import { Status } from '../types/Status'
+import { IManufacturer } from '../types/Manufacturer'
+import httpService from '../../services/http-service'
+
+export interface ManufacturerState {
+  manufacturer: IManufacturer[]
+  status: Status
+}
+
+export const initialState: ManufacturerState = { manufacturer: [], status: Status.idle }
+
+// The function below is called a thunk and allows us to perform async logic. It
+// can be dispatched like a regular action: `dispatch(blabal(10))`. This
+// will call the thunk with the `dispatch` function as the first argument. Async
+// code can then be executed and other actions can be dispatched. Thunks are
+// typically used to make async requests.
+export const getAllCompanies = createAsyncThunk('manufacturer/getAll', async () => {
+  const response = await httpService.get<IManufacturer[]>('/companies')
+  // The value we return becomes the `fulfilled` action payload
+  return response as unknown as IManufacturer[]
+})
+
+export const manufacturerSlice = createSlice({
+  name: 'manufacturer',
+  initialState,
+  // The `reducers` field lets us define reducers and generate associated actions
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getAllCompanies.pending, (state) => {
+        state.status = Status.loading
+        state.manufacturer = []
+      })
+      .addCase(getAllCompanies.rejected, (state) => {
+        state.status = Status.failed
+        state.manufacturer = []
+      })
+      .addCase(getAllCompanies.fulfilled, (state, action) => {
+        state.status = Status.idle
+        state.manufacturer = action.payload || []
+      })
+  },
+})
+
+export default manufacturerSlice.reducer
---
+++ src/redux/types/Manufacturer.ts
@@ -0,0 +1,11 @@
+
+export type IManufacturer = {
+  slug: string;
+  name: string;
+  address: string;
+  city: string;
+  state: string;
+  zip: string;
+  account: number;
+  contact: string;
+};
