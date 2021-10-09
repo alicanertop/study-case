185db320589a586c3f1b009af256141bed7f41f4
Alican ERTOP <alicanertop@gmail.com>
2021-10-09 22:58:11 +0300

    ManufacturerFilterParams type added

--- src/types/Manufacturer.ts
+++ src/types/Manufacturer.ts
@@ -1,11 +1,16 @@
+import { BaseFilterParams } from './BaseFilterParams'
 
 export type IManufacturer = {
-  slug: string;
-  name: string;
-  address: string;
-  city: string;
-  state: string;
-  zip: string;
-  account: number;
-  contact: string;
-};
+  slug: string
+  name: string
+  address: string
+  city: string
+  state: string
+  zip: string
+  account: number
+  contact: string
+}
+
+export type ManufacturerFilterParams = BaseFilterParams & {
+  name_like?: string
+}
