e152ea50bf6382dfc48dc738082f030d9683f77c
Alican ERTOP <alicanertop@gmail.com>
2021-10-09 14:26:50 +0300

    init

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  useTabs: false,
+  tabWidth: 2,
+  printWidth: 100,
+  semi: false,
+}
---
+++ src/constants/index.ts
@@ -0,0 +1,2 @@
+export { default as companies } from './companies.json'
+export { default as items } from './items.json'
