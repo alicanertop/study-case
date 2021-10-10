836b42dd7495aae5d2a2908dea12e52e0bae383d
Alican ERTOP <alicanertop@gmail.com>
2021-10-10 17:45:43 +0300

    ChoiceInput updated using for styled

---
+++ src/components/atoms/ChoiceInput/Container.tsx
@@ -0,0 +1,91 @@
+import styled from 'styled-components'
+import { svg } from '../../../constants/base64'
+
+type IContainer = {
+  componentName: string
+  type: 'radio' | 'checkbox'
+  size: string
+  disabled: boolean
+}
+const ChoiceInputStyle = {
+  checkbox: {
+    'margin-right': '8px',
+    'border-radius': '2px',
+    border: 'unset',
+    'box-shadow': '0px 1px 7px var(--purple500)',
+    'checked:background-color': 'var(--blue300)',
+    'disabled:background-color': 'var(--grayscale500)',
+    'disabled:border-color': '',
+    'disabled:color': 'var(--grayscale500)',
+  },
+  radio: {
+    'margin-right': '12px',
+    'border-radius': '17.5px',
+    border: '2px solid var(--blue300)',
+    'box-shadow': 'unset',
+    'checked:background-color': 'unset',
+    'disabled:background-color': 'var(--gray200)',
+    'disabled:border-color': 'var(--grayscale500)',
+    'disabled:color': 'var(--grayscale500)',
+  },
+}
+const Container = styled.div.attrs((props: IContainer) => ({ ...props }))`
+  width: 100%;
+
+  :not(:last-child) {
+    margin-bottom: 18px;
+  }
+
+  & .${(props) => `${props.componentName}__container`} {
+    display: flex;
+    align-items: center;
+    & .visually-hidden {
+      display: none;
+      pointer-events: none;
+    }
+  }
+
+  & .${(props) => `${props.componentName}__label-text`} {
+    cursor: pointer;
+    font-size: 14px;
+    user-select: none;
+    line-height: 18px;
+    font-style: normal;
+    font-weight: normal;
+    font-family: Open Sans;
+    letter-spacing: 0.16px;
+  }
+
+  & .${(props) => `${props.componentName}__overlay`} {
+    background-color: #fff;
+    width: ${({ size }) => size};
+    height: ${({ size }) => size};
+    margin-right: ${({ type }) => ChoiceInputStyle[type]['margin-right']};
+    border-radius: ${({ type }) => ChoiceInputStyle[type]['border-radius']};
+    border: ${({ type }) => ChoiceInputStyle[type]['border']};
+    box-shadow: ${({ type }) => ChoiceInputStyle[type]['box-shadow']};
+  }
+
+  & .${(props) => `${props.componentName}__input:checked + .${props.componentName}__overlay`} {
+    background: ${({ type }) => svg[type]} center no-repeat;
+    background-color: ${({ type }) => ChoiceInputStyle[type]['checked:background-color']};
+  }
+
+  &
+    .${(props) =>
+      `${props.componentName}__input:disabled:checked + .${props.componentName}__overlay`} {
+    background-color: ${({ type }) => ChoiceInputStyle[type]['disabled:background-color']};
+  }
+
+  & .${(props) => `${props.componentName}__input:disabled + .${props.componentName}__overlay`} {
+    background-color: ${({ type }) => ChoiceInputStyle[type]['disabled:background-color']};
+    border-color: ${({ type }) => ChoiceInputStyle[type]['disabled:border-color']};
+  }
+
+  & .${(props) => `${props.componentName}__label-text`} {
+    cursor: ${({ disabled }) => (disabled ? 'default' : 'pointer')};
+    color: ${({ disabled, type }) =>
+      disabled ? ChoiceInputStyle[type]['disabled:color'] : 'unset'};
+  }
+`
+export default Container
--- src/components/atoms/ChoiceInput/index.tsx
+++ src/components/atoms/ChoiceInput/index.tsx
@@ -1,46 +1,67 @@
 import React from 'react'
 import classnames from 'classnames'
-
-import './choice-input.scss'
+import Container from './Container'
 
 export interface IBaseSelectableInput
   extends React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> {
-  containerProps?: React.HtmlHTMLAttributes<HTMLLabelElement>
+  containerProps?: typeof Container
+  labelProps?: React.HtmlHTMLAttributes<HTMLLabelElement>
   overlayProps?: React.HtmlHTMLAttributes<HTMLSpanElement>
   labelTextProps?: React.HtmlHTMLAttributes<HTMLSpanElement>
   labelText?: string
   type: 'checkbox' | 'radio'
+  componentSize?: string
+  componentName?: string
 }
 
 const BaseSelectableInput = React.forwardRef<HTMLInputElement, IBaseSelectableInput>(
-  (
-    { containerProps, overlayProps, labelTextProps, labelText, type, id, ...rest },
-    ref: React.LegacyRef<HTMLInputElement> | undefined,
-  ) => {
+  (props, ref: React.LegacyRef<HTMLInputElement> | undefined) => {
+    const {
+      componentName = 'choice-input',
+      componentSize = '22px',
+      containerProps,
+      labelProps,
+      overlayProps,
+      labelTextProps,
+      labelText,
+      disabled,
+      type,
+      id,
+      ...rest
+    } = props
+
     return (
-      <label
+      <Container
         {...containerProps}
-        htmlFor={id}
-        className={classnames(`choice-input__container ${type}`, containerProps?.className)}>
-        <input
-          {...rest}
-          ref={ref}
-          id={id}
-          type={type}
-          className={classnames(`choice-input__input visually-hidden ${type}`, rest?.className)}
-        />
-        <span
-          {...overlayProps}
-          className={classnames(`choice-input__overlay ${type}`, overlayProps?.className)}
-        />
-        {labelText && (
+        type={type}
+        disabled={disabled}
+        size={componentSize}
+        componentName={componentName}>
+        <label
+          {...labelProps}
+          htmlFor={id}
+          className={classnames(`${componentName}__container`, labelProps?.className)}>
+          <input
+            {...rest}
+            ref={ref}
+            id={id}
+            type={type}
+            disabled={disabled}
+            className={classnames(`${componentName}__input visually-hidden`, rest?.className)}
+          />
           <span
-            {...labelTextProps}
-            className={classnames(`choice-input__label-text ${type}`, labelTextProps?.className)}>
-            {labelText}
-          </span>
-        )}
-      </label>
+            {...overlayProps}
+            className={classnames(`${componentName}__overlay`, overlayProps?.className)}
+          />
+          {labelText && (
+            <span
+              {...labelTextProps}
+              className={classnames(`${componentName}__label-text`, labelTextProps?.className)}>
+              {labelText}
+            </span>
+          )}
+        </label>
+      </Container>
     )
   },
 )
