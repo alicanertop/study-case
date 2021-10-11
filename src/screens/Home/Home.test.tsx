f5866a459a716c5f19cd11fbd5952f2b07585a34
Alican ERTOP <alicanertop@gmail.com>
2021-10-11 06:17:02 +0300

    home page layout updated

--- src/screens/Home/Home.test.tsx
+++ src/screens/Home/Home.test.tsx
@@ -1,15 +1,14 @@
-import React from 'react'
 import { render } from '@testing-library/react'
 import { Provider } from 'react-redux'
 import { store } from '../../redux/store'
 import Home from './Home'
 
-test('renders learn react link', () => {
+test('renders home react link', () => {
   const { getByText } = render(
     <Provider store={store}>
       <Home />
     </Provider>,
   )
 
-  expect(getByText(/learn/i)).toBeInTheDocument()
+  expect(getByText(/home/i)).toBeInTheDocument()
 })
--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,7 +1,25 @@
 import './Home.scss'
 
+import { BrandFilter, Sorting, TagFilter, ProductList } from '../../components/organisms'
+import { Footer, Header } from '../../containers'
+
 function Home() {
-  return <div>Home</div>
+  return (
+    <>
+      <Header />
+      <div className="home-content">
+        <div className="home-filter">
+          <Sorting />
+          <BrandFilter />
+          <TagFilter />
+        </div>
+        <div className="home-product-list">
+          <ProductList />
+        </div>
+      </div>
+      <Footer />
+    </>
+  )
 }
 
 export default Home
