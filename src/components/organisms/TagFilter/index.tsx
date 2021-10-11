5803ba268b2bc99cc64a5b2292522b9e568d4051
Alican ERTOP <alicanertop@gmail.com>
2021-10-11 03:04:17 +0300

    TagFilter implemented

---
+++ src/components/organisms/TagFilter/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+import { itemReducer, tagReducer, useAppDispatch, useAppSelector } from '../../../redux'
+import { SeachableFilter, SeachableFilterItem } from '../../molecues'
+
+function TagFilter() {
+  const dispatch = useAppDispatch()
+  const { status, tagList } = useAppSelector(({ tag }) => tag)
+
+  const [tagName, setTagName] = React.useState('')
+  const [selectedItem, setSelectedItem] = React.useState<SeachableFilterItem[]>([])
+
+  React.useEffect(() => {
+    dispatch(
+      itemReducer.setFilterParams({
+        tags_like: selectedItem.length ? selectedItem.map((i) => i.value).join(',') : undefined,
+      }),
+    )
+  }, [selectedItem])
+
+  React.useEffect(() => {
+    dispatch(tagReducer.getTags({ name_like: tagName ? tagName : undefined }))
+  }, [tagName])
+
+  return (
+    <SeachableFilter
+      label="Tags"
+      disableShowCount
+      status={status}
+      placeholder="Search in Tags"
+      onSearch={(e) => setTagName(e)}
+      onSelect={(e) => setSelectedItem(e)}
+      data={tagList as SeachableFilterItem[]}
+    />
+  )
+}
+
+export default TagFilter
