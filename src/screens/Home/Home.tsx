1a6e9249e8669e85f462e0036f1d878d9013035e
Alican ERTOP <alicanertop@gmail.com>
2021-10-26 01:46:43 +0300

    quick filter responsive

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,14 +1,27 @@
-import './Home.scss'
+import React from 'react'
+import classnames from 'classnames'
 
 import { BrandFilter, Sorting, TagFilter, ProductList } from '../../components/organisms'
 import { Footer, Header } from '../../containers'
 
+import Close from '../../assets/img/close.svg'
+import RightArrow from '../../assets/img/right-arrow.svg'
+
+import './Home.scss'
+
 function Home() {
+  const [active, toggleActive] = React.useState(false)
+  const handleToggle = () => toggleActive((prev) => !prev)
   return (
     <>
       <Header />
       <div className="home-content">
-        <div className="home-filter">
+        <div className={classnames('home-filter', { active })}>
+          <div className={classnames('button__container', { active })}>
+            <button className="button" onClick={handleToggle}>
+              <img className="icon" src={active ? Close : RightArrow} />
+            </button>
+          </div>
           <Sorting />
           <BrandFilter />
           <TagFilter />
